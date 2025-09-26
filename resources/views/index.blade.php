03bc50ea295423efc48c6065b9ae5ccf95af5521
Achmad Naufal Ferdiansyah <naufansyah31@gmail.com>
2025-09-26 20:05:21 +0700

    landing page design

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,7 +7,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Base - Tailwind CSS Startup Template</title>
   <link rel="icon" href="favicon.ico">
+  <script src="https://unpkg.com/feather-icons"></script>
   <link rel="stylesheet" href="../assets/styles/style.css" />
+  <link href="{{ asset('fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
 </head>
 
 <body x-data="{ page: 'home', 'darkMode': true, 'stickyMenu': false, 'navigationOpen': false, 'scrollTop': false }"
@@ -20,10 +22,7 @@
     @scroll.window="stickyMenu = (window.pageYOffset > 20) ? true : false">
     <div class="bb ze ki xn 2xl:ud-px-0 oo wf yf i">
       <div class="vd to/4 tc wf yf">
-        <a href="index.html">
-          <img class="om" src="images/logo-light.svg" alt="Logo Light" />
-          <img class="xc nm" src="images/logo-dark.svg" alt="Logo Dark" />
-        </a>
+        <a href="index.html" class="ek yj go kk wm xb">Psikolog</a>
 
         <!-- Hamburger Toggle BTN -->
         <button class="po rc" @click="navigationOpen = !navigationOpen">
@@ -39,7 +38,6 @@
             </span>
           </span>
         </button>
-        <!-- Hamburger Toggle BTN -->
       </div>
 
       <div class="vd wo/4 sd qo f ho oo wf yf" :class="{ 'd hh rm sr td ud qg ug jc yh': navigationOpen }">
@@ -64,7 +62,7 @@
                 <li><a href="blog-grid.html" class="xl" :class="{ 'mk': page === 'blog-grid' }">Blog Grid</a></li>
                 <li><a href="blog-single.html" class="xl" :class="{ 'mk': page === 'blog-single' }">Blog Single</a></li>
                 <li><a href="signin.html" class="xl" :class="{ 'mk': page === 'signin' }">Sign In</a></li>
-                <li><a href="signup.html" class="xl" :class="{ 'mk': page === 'signup' }">Sign Up</a></li>
+                <li><a href="signup.html" class="xl" :class="{ 'nk': page === 'signup' }">Sign Up</a></li>
                 <li><a href="404.html" class="xl" :class="{ 'mk': page === '404' }">404</a></li>
               </ul>
               <!-- Dropdown End -->
@@ -78,7 +76,7 @@
             <label class="rc ab i">
               <input type="checkbox" :value="darkMode" @change="darkMode = !darkMode" class="pf vd yc uk h r za ab" />
               <!-- Icon Sun -->
-              <svg :class="{ 'wn' : page === 'home', 'xh' : page === 'home' && stickyMenu }" class="th om" width="25"
+              <svg :class="{ 'wn': page === 'home' && !stickyMenu && darkMode, 'xh': page === 'home' && stickyMenu }" class="th om" width="25"
                 height="25" viewBox="0 0 25 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path
                   d="M12.0908 18.6363C10.3549 18.6363 8.69 17.9467 7.46249 16.7192C6.23497 15.4916 5.54537 13.8268 5.54537 12.0908C5.54537 10.3549 6.23497 8.69 7.46249 7.46249C8.69 6.23497 10.3549 5.54537 12.0908 5.54537C13.8268 5.54537 15.4916 6.23497 16.7192 7.46249C17.9467 8.69 18.6363 10.3549 18.6363 12.0908C18.6363 13.8268 17.9467 15.4916 16.7192 16.7192C15.4916 17.9467 13.8268 18.6363 12.0908 18.6363ZM12.0908 16.4545C13.2481 16.4545 14.358 15.9947 15.1764 15.1764C15.9947 14.358 16.4545 13.2481 16.4545 12.0908C16.4545 10.9335 15.9947 9.8236 15.1764 9.00526C14.358 8.18692 13.2481 7.72718 12.0908 7.72718C10.9335 7.72718 9.8236 8.18692 9.00526 9.00526C8.18692 9.8236 7.72718 10.9335 7.72718 12.0908C7.72718 13.2481 8.18692 14.358 9.00526 15.1764C9.8236 15.9947 10.9335 16.4545 12.0908 16.4545ZM10.9999 0.0908203H13.1817V3.36355H10.9999V0.0908203ZM10.9999 20.8181H13.1817V24.0908H10.9999V20.8181ZM2.83446 4.377L4.377 2.83446L6.69082 5.14828L5.14828 6.69082L2.83446 4.37809V4.377ZM17.4908 19.0334L19.0334 17.4908L21.3472 19.8046L19.8046 21.3472L17.4908 19.0334ZM19.8046 2.83337L21.3472 4.377L19.0334 6.69082L17.4908 5.14828L19.8046 2.83446V2.83337ZM5.14828 17.4908L6.69082 19.0334L4.377 21.3472L2.83446 19.8046L5.14828 17.4908ZM24.0908 10.9999V13.1817H20.8181V10.9999H24.0908ZM3.36355 10.9999V13.1817H0.0908203V10.9999H3.36355Z"
@@ -89,9 +87,9 @@
             </label>
           </div>
 
-          <a href="signin.html" :class="{ 'nk yl' : page === 'home', 'ok' : page === 'home' && stickyMenu }"
+          <a href="signin.html" :class="{ 'lk' : page === 'home', 'ok' : page === 'home' && stickyMenu }"
             class="ek pk xl">Sign In</a>
-          <a href="signup.html" :class="{ 'hh/[0.15]' : page === 'home', 'sh' : page === 'home' && stickyMenu }"
+          <a href="signup.html" :class="{ 'nk' : page === 'home', 'sh' : page === 'home' && stickyMenu }"
             class="lk gh dk rg tc wf xf _l gi hi">Sign Up</a>
         </div>
       </div>
@@ -104,30 +102,25 @@
     <!-- ===== Hero Start ===== -->
     <section class="gj do ir hj sp jr i pg">
       <!-- Hero Images -->
-      <div class="xc fn zd/2 2xl:ud-w-187.5 bd 2xl:ud-h-171.5 h q r">
+      {{-- <div class="xc fn zd/2 2xl:ud-w-187.5 bd 2xl:ud-h-171.5 h q r">
         <img src="images/shape-01.svg" alt="shape" class="xc 2xl:ud-block h t -ud-left-[10%] ua" />
         <img src="images/shape-02.svg" alt="shape" class="xc 2xl:ud-block h u p va" />
         <img src="images/shape-03.svg" alt="shape" class="xc 2xl:ud-block h v w va" />
         <img src="images/shape-04.svg" alt="shape" class="h q r" />
         <img src="images/hero.png" alt="Woman" class="h q r ua" />
-      </div>
+      </div> --}}
 
       <!-- Hero Content -->
       <div class="bb ze ki xn 2xl:ud-px-0">
         <div class="tc _o">
           <div class="animate_left jn/2">
-            <h1 class="fk vj zp or kk wm wb">We specialize in UI/UX, Web Development, Digital Marketing.</h1>
+            <h1 class="fk vj zp or kk wm wb">Kesehatan Mental Anda Prioritas Kami</h1>
             <p class="fq">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque fringilla magna mauris. Nulla fermentum
-              viverra sem eu rhoncus consequat varius nisi quis, posuere magna.
+              Temukan solusi terbaik untuk kesehatan mental Anda bersama psikolog profesional
             </p>
 
             <div class="tc tf yo zf mb">
               <a href="#!" class="ek jk lk gh gi hi rg ml il vc _d _l">Get Started Now</a>
-
-              <span class="tc sf">
-                <a href="#!" class="inline-block ek xj kk wm"> Call us (0123) 456 – 789 </a>
-                <span class="inline-block">For any question or concern</span>
               </span>
             </div>
           </div>
@@ -408,11 +401,12 @@
       <div class="bb ze ki xn yq mb en">
         <div class="wc qf pn xo ng">
           <!-- Service Item -->
-          <div class="animate_top sg oi pi zq ml il am cn _m">
-            <img src="images/icon-04.svg" alt="Icon" />
-            <h4 class="ek zj kk wm nb _b">Crafted for Startups</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.</p>
+         <a href="">
+           <div class="animate_top sg oi pi zq ml il am cn _m">
+            <img class="ce ed" src="images/newspaper-svgrepo-com.svg" alt="Icon" />
+            <h4 class="ek zj kk wm nb _b">Konsultasi</h4>
           </div>
+         </a>
 
           <!-- Service Item -->
           <div class="animate_top sg oi pi zq ml il am cn _m">
